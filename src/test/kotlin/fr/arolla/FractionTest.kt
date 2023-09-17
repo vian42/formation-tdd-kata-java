43b87346bd7942808295cf794b936e8dbbcd9923
Vianney M <vianney.maynadier@arolla.fr>
2023-09-17 21:24:35 +0200

    re factorisation, create a constant in the test class

--- src/test/kotlin/fr/arolla/FractionTest.kt
+++ src/test/kotlin/fr/arolla/FractionTest.kt
@@ -5,33 +5,30 @@ import org.junit.jupiter.api.Test
 
 class FractionTest {
 
+    private val one = Fraction(1, 1)
+
     @Test
     internal fun `the fraction have one num and one dem`() {
-        val frac = Fraction(1,1)
-        assertThat(frac.num).isEqualTo(1)
-        assertThat(frac.dem).isEqualTo(1)
+        assertThat(one.num).isEqualTo(1)
+        assertThat(one.dem).isEqualTo(1)
     }
 
     @Test
     internal fun `the sum of  two fraction is a fraction`() {
-        val firstFraction = Fraction(1,1)
-        val secondFraction = Fraction(1,1)
-        val result = firstFraction.sum(secondFraction)
+        val result = one.sum(one)
         assertThat(result::class).isEqualTo(Fraction::class)
     }
 
     @Test
     internal fun `the sum of 1 plus 1 return 2`() {
-        val one = Fraction(1,1)
         val two = one.sum(one)
-        assertThat(two).isEqualTo(Fraction(2,1))
+        assertThat(two).isEqualTo(Fraction(2, 1))
     }
 
     @Test
     internal fun `can add a real fraction to 1`() {
-        val one = Fraction(1,1)
-        val fraction  = Fraction(1,4)
+        val fraction = Fraction(1, 4)
         val result = one.sum(fraction)
-        assertThat(result).isEqualTo(Fraction(5,4))
+        assertThat(result).isEqualTo(Fraction(5, 4))
     }
 }
