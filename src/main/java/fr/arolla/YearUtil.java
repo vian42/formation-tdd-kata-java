cb02df32758ae7af75ad1065111810128b4b2466
Vianney M <vianney.maynadier@arolla.fr>
2023-09-18 11:28:24 +0200

    write the rules on the divisibility by 100 and 400

--- src/main/java/fr/arolla/YearUtil.java
+++ src/main/java/fr/arolla/YearUtil.java
@@ -2,10 +2,16 @@ package fr.arolla;
 
 public class YearUtil {
     public static Boolean isLeap(Integer year) {
-        if (year == 1900) {
-            return false;
-        }
-        return isDivisibleBy4(year);
+        return isDivisibleBy4(year)
+                && (!isDivisibleBy100(year) || isDivisibleBy400(year));
+    }
+
+    private static boolean isDivisibleBy400(Integer year) {
+        return year % 400 == 0;
+    }
+
+    private static boolean isDivisibleBy100(Integer year) {
+        return year % 100 == 0;
     }
 
     private static boolean isDivisibleBy4(Integer year) {
--- src/test/java/fr/arolla/LeapYearTest.java
+++ src/test/java/fr/arolla/LeapYearTest.java
@@ -1,6 +1,5 @@
 package fr.arolla;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,17 +16,22 @@ public class LeapYearTest {
         assertThat(isLeap).isTrue();
     }
     @ParameterizedTest
+    @CsvSource({"2000","1600"})
+    void the_year_divisible_by_400_are_leap(Integer year) {
+        Boolean isLeap = YearUtil.isLeap(year);
+
+        assertThat(isLeap).isTrue();
+    }
+    @ParameterizedTest
     @CsvSource({"2021","2023","2029"})
     void the_year_not_divisible_by_4_are_not_leap(Integer year) {
         Boolean isLeap = YearUtil.isLeap(year);
 
         assertThat(isLeap).isFalse();
     }
-
-    @Test
-    void the_year_1900_is_not_a_leap_year() {
-        Integer year = 1900;
-
+    @ParameterizedTest
+    @CsvSource({"1900","2100","1700"})
+    void the_year_divisible_by_100_are_not_leap(Integer year) {
         Boolean isLeap = YearUtil.isLeap(year);
 
         assertThat(isLeap).isFalse();
